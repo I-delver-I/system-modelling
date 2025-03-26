f6e7938d66f35317b74572673d9a8a1d8d8281f5
delver <dima.plugaturevchik@gmail.com>
2025-03-26 14:41:00 +0200

    update simulation.py to add modified simulation function and improve element naming; enhance .gitignore for JetBrains settings

--- cp 2/simulation.py
+++ cp 2/simulation.py
@@ -6,7 +6,7 @@ from process import ProcessElement
 from simulation_model import Model
 
 
-def run_simulation() -> None:
+def run_modified_simulation() -> None:
     """
     Приклад запуску моделі:
       - Один елемент CreateElement (джерело подій),
@@ -14,20 +14,24 @@ def run_simulation() -> None:
       - 10% заявок із третього процесу повертаються до першого.
     """
     # Параметри створення (середній інтервал 0.2)
-    create1 = CreateElement(get_delay=partial(random.expovariate, lambd=1.0 / 0.2))
+    create1 = CreateElement(get_delay=partial(random.expovariate, lambd=1.0 / 0.2), 
+                            name='CreateElement1')
 
     # Параметри процесів
     process1 = ProcessElement(
+        name='ProcessElement1',
         max_queue_size=10,
         num_handlers=5,
         get_delay=partial(random.expovariate, lambd=1.0 / 1.2)
     )
     process2 = ProcessElement(
+        name='ProcessElement2',
         max_queue_size=8,
         num_handlers=7,
         get_delay=partial(random.expovariate, lambd=1.0 / 2.0)
     )
     process3 = ProcessElement(
+        name='ProcessElement3',
         max_queue_size=1,
         num_handlers=2,
         get_delay=partial(random.expovariate, lambd=1.0 / 1.0)
@@ -47,7 +51,47 @@ def run_simulation() -> None:
     print('Final statistics:')
     for name, element_stats in stats.items():
         print(f'{name}:\n{element_stats}\n')
+        
+def run_simulation() -> None:
+    """
+    Приклад запуску моделі:
+      - Один елемент CreateElement (джерело подій),
+      - Три послідовні ProcessElement із різними параметрами.
+    """
+    create1 = CreateElement(get_delay=partial(random.expovariate, lambd=1.0 / 0.2))
+    
+    # Параметри процесів
+    process1 = ProcessElement(
+        max_queue_size=10,
+        get_delay=partial(random.expovariate, lambd=1.0 / 1.2)
+    )
+    process2 = ProcessElement(
+        max_queue_size=8,
+        get_delay=partial(random.expovariate, lambd=1.0 / 2.0)
+    )
+    process3 = ProcessElement(
+        max_queue_size=1,
+        get_delay=partial(random.expovariate, lambd=1.0 / 1.0)
+    )
+
+    # З’єднання блоків
+    create1.add_next_element(process1)
+    process1.add_next_element(process2)
+    process2.add_next_element(process3)
+
+    # Створюємо модель та запускаємо
+    model = Model(parent=create1)
+    stats = model.simulate(end_time=1000, verbose=False)
+
+    print('Final statistics:')
+    for name, element_stats in stats.items():
+        print(f'{name}:\n{element_stats}\n')
 
 
 if __name__ == '__main__':
+    print('|Simulation|')
     run_simulation()
+    print('|Modified simulation|')
+    run_modified_simulation()
+    
+    
